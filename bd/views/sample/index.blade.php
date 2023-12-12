92d3c63932976a7aa97d29462557bac405c68255
uniho <uniho@users.noreply.github.com>
2023-12-13 05:03:40 +0900

    (chore)upgrade samples

--- bd/views/sample/index.blade.php
+++ bd/views/sample/index.blade.php
@@ -1,31 +1,118 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="initial-scale=1, width=device-width" />
-    <title>{{ HQ::getenv('CCC::APP_NAME') }}</title>
-    <link rel="stylesheet" href="fd/css/normalize.css">
-    <link rel="stylesheet" href="fd/css/preflight.css">
-    <link rel="stylesheet" href="fd/css/style.css">
+@php
+  $INTERVAL = 10;
+
+  if (request()->isMethod('post')) {
+    if (!\Unsta\FloodControl::isAllowed(request()->url().'::message', 1, $INTERVAL)) {
+      $query = request()->query();
+      $query['error'] = "You can only use it once every $INTERVAL seconds.";
+      header('Location: '.request()->url().'?'.Arr::query($query));
+      return;
+    }
+
+    $message = request()->input('message');
+    if (!$message) {
+      $query = request()->query();
+      $query['error'] = 'No message'; 
+      header('Location: '.request()->url().'?'.Arr::query($query));
+      return;
+    }
+
+    \Unsta\FloodControl::register(request()->url().'::message', $INTERVAL);
+    // request()->session()->regenerateToken();
+
+    $uuid = (string)Str::uuid();
+    Cache::put(request()->url().$uuid, $message, 30);
+    $query = request()->query();
+    $query['uuid'] = $uuid; 
+    header('Location: '.request()->url().'?'.Arr::query($query));
+    return;
+  }
+
+  $message = false;
+  if (request()->has('uuid')) {
+    $message = Cache::pull(request()->url().request()->input('uuid'));
+    if (!$message) {
+      $query = request()->query();
+      unset($query['uuid']);
+      $query['error'] = "This message will self-destruct."; 
+      header('Location: '.request()->url().'?'.Arr::query($query));
+      return;
+    }
+  }
+
+  $error = request()->query('error');
+
+@endphp
+
+<x-sample.html>
+  <x-slot name="title">
+    {{ HQ::getenv('CCC::APP_NAME') }} 
+  </x-slot>  
+
+  <x-slot name="header">
     <link rel="stylesheet" href="?css_route=sample.style">
-  </head>
-  <body>
-    <div class="wrapper">
-      <div class="title">
-        {{ HQ::getenv('CCC::APP_NAME') }} 
-      </div>  
+    <style>
+      .color-box {
+        border: yellow 1px solid;
+        padding: 1rem;
+      }
+      textarea {
+        width: 100%;
+      }
+    </style>
+  </x-slot>  
+
+  {{-- slot --}}
+  <div class="wrapper">
+    <div class="title">
+      {{ HQ::getenv('CCC::APP_NAME') }} 
+    </div>  
+
+    @if(!$error)
+      @if($message === false)
+        <div>
+          <form action="{{ '?'.Arr::query(request()->query()) }}" method="POST">
+            @csrf
+            <textarea rows="6" name="message"></textarea>
+            <button type="submit">
+              SEND MESSAGE
+            </button>
+          </form>
+        </div>
+
+        <div>
+          Your Query(JSON) =>
+          <pre class="color-box">{{ json_encode(request()->query(), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT) }}</pre>
+        </div>
+        
+        <div>
+          Your Query(YAML) =>
+          <pre class="color-box">{{ Symfony\Component\Yaml\Yaml::dump(request()->query()) }}</pre>
+        </div>
+        
+        <div>
+          Markdown =>
+          <div class="color-box">
+            {!! Compilers::markdown('sample.test', ['test' => 2]) !!}
+          </div>
+        </div>
+      @else
+        <div>
+          Your Message =>
+          <pre class="color-box">{{ $message }}</pre>
+          <div>
+            Please go back.
+          </div>
+        </div>
+      @endif
+    @else
+      Error: 
       <div>
-        Your Query => <br/>
-        {{ json_encode(request()->query(), JSON_UNESCAPED_UNICODE) }}
+        {{ $error }}
       </div>
       <div>
-        Your Content => <br/>
-        {{ request()->getContent() }}
+        Please go back.
       </div>
-      <div>
-        Markdown => <br/>
-        {!! Compilers::markdown('sample.test', ['test' => 2]) !!}
-      </div>
-    </div>
-  </body>
-</html>
+    @endif
+  </div>
+</x-sample.html>
