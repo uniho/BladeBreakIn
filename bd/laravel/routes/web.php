01cc7f95f4f6a08f778a9a68f0d142a6390d6cc9
uniho <uniho@users.noreply.github.com>
2024-04-18 20:12:52 +0900

    (Laravel)Bump Laravel to v11.4.0

--- bd/laravel/routes/web.php
+++ bd/laravel/routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 \Route::match(['get', 'post'], '/', function (Request $request) {
   return \HQ::webOrigin($request);
 });
