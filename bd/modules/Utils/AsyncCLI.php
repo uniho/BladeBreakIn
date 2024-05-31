f2b07179bc28efe22e0183d6b12f31b045406621
uniho <uniho@users.noreply.github.com>
2024-05-31 18:44:31 +0900

    cleanup async cli commands

--- bd/modules/Utils/AsyncCLI.php
+++ bd/modules/Utils/AsyncCLI.php
@@ -4,17 +4,10 @@ namespace Utils;
 
 final class AsyncCLI
 {
-  public static function runQueueWorker()
+  public static function runArtisan($params = '')
   {
     $php = \HQ::getenv('CCC::PHP_CLI');
-    $cmd = __DIR__ . '/../../async/cli/queue_work.php';
-    exec("$php $cmd > /dev/null &");
-  }
-
-  public static function pruneBatches()
-  {
-    $php = \HQ::getenv('CCC::PHP_CLI');
-    $cmd = __DIR__ . '/../../async/cli/prune_batches.php';
+    $cmd = __DIR__ . '/../../async/cli/artisan.php ' . $params;
     exec("$php $cmd > /dev/null &");
   }
 }
